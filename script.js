1fa3ac437fe4d6cc39bd6bded50000aeb36ec117
9kartik <mauryakartik@gmail.com>
2017-02-02 21:36:28 +0530

    multi comments

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 var video = document.getElementById("player"), track; 
+var comments={}
+var cue;
 video.addEventListener("loadedmetadata", function() { 
    track = this.addTextTrack("captions", "English", "en"); 
-   track.mode="visible";
-   console.log(track);
+	console.log(track);
+   track.mode="showing";
 });
+cue=(tm,dur,cmt)=>track.addCue(new VTTCue(tm, tm+dur,cmt));
+function addComment()
+	{
+		let x=0;
+		console.log('stuff');
+		if(!comments[player.currentTime])
+			comments[player.currentTime]=[cmnt.value]
+		else {x+=1;
+			  comments[player.currentTime].push(cmnt.value);}
+		if(player.paused)
+			cue(player.currentTime,1,`<span style="background:hsl(${x*10}%,40%,50%)">${cmnt.value}</span>`)
+	}
